4a1f37d8a5e78f989b2b4e1a11eac1a5f7815dec
AounBoss <aoun8221@gmail.com>
2024-10-16 10:22:58 +0500

    Add files via upload

---
+++ AddItem.java
@@ -0,0 +1,13 @@
+package lab10;
+import java.util.ArrayList;
+import java.util.List;
+public class AddItem { 
+ public static void main(String[] args) { 
+ List <String> cars = new ArrayList<String>();
+ cars.add("Volvo");
+ cars.add("BMW");
+ cars.add("Ford");
+ cars.add("Mazda");
+ System.out.println(cars);
+ } 
+}
